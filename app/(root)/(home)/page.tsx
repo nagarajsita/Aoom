8262877ca1690ed93c197b618b33726c216cb621
nagarajsita <nagarajsita@gmail.com>
2024-08-24 14:11:14 +0530

    time formatted

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,18 +4,18 @@ import React from "react";
 
 const Home = () => {
   const now = new Date();
-  const time = now.toLocaleTimeString("en-US", {
+  const time = now.toLocaleTimeString("en-IN", {
     hour: "2-digit",
     minute: "2-digit",
   });
-  const date = now.toLocaleDateString("en-US", { dateStyle: "full" });
+  const date = now.toLocaleDateString("en-IN", { dateStyle: "full"});
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md: px-5 max-md:py-8 lg:p-11">
           <NextUpcomingCall />
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time.toUpperCase()}</h1>
             <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
         </div>
