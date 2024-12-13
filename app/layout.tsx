c4fe485fc8cb6caff391b3760305d2d5f33af8e4
nagarajsita <nagarajsita@gmail.com>
2024-12-13 14:08:40 +0530

    googel-search-console-meta-added

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="Xn8g41V3TKqHic6My1v-unRjX3HbXXQ3IizIRQsL6HM" />
+      </head>
       <ClerkProvider
         appearance={{
           layout: {
